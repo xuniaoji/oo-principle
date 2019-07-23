019d2358d41e4b6eafac28fb1026249a1a8ed06f
shuming-xu <shuming.xu@rea-group.com>
2019-07-23 19:03:31 +0800

    implemented first case

---
+++ src/test/java/ReactangleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class ReactangleTest {
+    @Test
+    public void shouldReturn200WhenExecuteSettingWidth200AndExcuteGetWidthGivenWidth100(){
+        int newWidth = 0;
+        assertEquals(200,newWidth);
+    }
+}
