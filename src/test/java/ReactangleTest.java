f88b0db0720c0c4947a8dde85d85515b62cb1e55
shuming-xu <shuming.xu@rea-group.com>
2019-07-23 19:36:09 +0800

    finish case 2.1

--- src/test/java/ReactangleTest.java
+++ src/test/java/ReactangleTest.java
@@ -11,7 +11,6 @@ public class ReactangleTest {
         try {
             rectangle.setWidth(200);
         } catch (Exception e) {
-            e.printStackTrace();
         }
         assertEquals(200,rectangle.getWidth());
     }
@@ -22,31 +21,39 @@ public class ReactangleTest {
         try {
             rectangle.setWidth(-200);
         } catch (Exception e) {
-            e.printStackTrace();
             assertEquals("invalid number",e.getMessage());
         }
     }
 
     @Test
-    public void should0WhenExecuteSettingWidth0AndExcuteGetWidthGivenWidth100(){
+    public void shouldReturn0WhenExecuteSettingWidth0AndExcuteGetWidthGivenWidth100(){
         RectangleTDD rectangle = new RectangleTDD(100,0);
         try {
             rectangle.setWidth(0);
         } catch (Exception e) {
-            e.printStackTrace();
-            assertEquals(0,rectangle.getWidth());
         }
+        assertEquals(0,rectangle.getWidth());
     }
 
     @Test
-    public void shouldReturn100WhenExecuteSettingHeight100AndExcuteGetHeightGivenWidth50(){
+    public void shouldReturn100WhenExecuteSettingHeight100AndExcuteGetHeightGivenHeight50(){
         RectangleTDD rectangle = new RectangleTDD(0,50);
         try {
             rectangle.setHeight(100);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) {}
         assertEquals(100,rectangle.getHeight());
     }
+
+    @Test
+    public void shouldThrowErrorWhenExecuteSettingHeightMinus200AndExcuteGetHeightGivenHeight100(){
+        RectangleTDD rectangle = new RectangleTDD(0,100);
+        try {
+            rectangle.setHeight(-200);
+        } catch (Exception e) {
+            assertEquals("invalid number",e.getMessage());
+        }
+    }
+
+
 
 }
