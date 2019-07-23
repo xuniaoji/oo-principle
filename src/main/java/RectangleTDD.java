edf654852358d5ec4400ef71f6427e19c9386428
shuming-xu <shuming.xu@rea-group.com>
2019-07-23 19:10:19 +0800

    finish first case

---
+++ src/main/java/RectangleTDD.java
@@ -0,0 +1,15 @@
+public class RectangleTDD {
+    int width;
+
+    public RectangleTDD(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
--- src/test/java/ReactangleTest.java
+++ src/test/java/ReactangleTest.java
@@ -1,12 +1,14 @@
 import org.junit.Test;
 
 
+
 import static org.junit.Assert.assertEquals;
 
 public class ReactangleTest {
     @Test
     public void shouldReturn200WhenExecuteSettingWidth200AndExcuteGetWidthGivenWidth100(){
-        int newWidth = 0;
-        assertEquals(200,newWidth);
+        RectangleTDD rectangle = new RectangleTDD(100);
+        rectangle.setWidth(200);
+        assertEquals(200,rectangle.getWidth());
     }
 }
